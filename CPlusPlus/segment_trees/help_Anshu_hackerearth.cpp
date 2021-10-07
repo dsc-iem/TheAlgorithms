917989f3679fc73264824351bbd119d3b35c46d7
Tushar Agarwal <dhorse619@gmail.com>
2021-10-08 00:51:57 +0530

    Update help_Anshu_hackerearth.cpp

--- CPlusPlus/segment_trees/help_Anshu_hackerearth.cpp
+++ CPlusPlus/segment_trees/help_Anshu_hackerearth.cpp
@@ -1,30 +1,14 @@
+/*Problem Link:-https://www.hackerearth.com/practice/data-structures/advanced-data-structures/fenwick-binary-indexed-trees/practice-problems/algorithm/help-ashu-1/*/
+
+/*Solution Link for ref:-https://www.youtube.com/watch?v=P8zS9SFMJwk&list=PL2q4fbVm1Ik6v2-emg_JGcC9v2v2YTbvq&index=7 */
+
 #include <bits/stdc++.h>
-#define int 				long long
-#define ll          long long
-#define pb 					push_back
-#define endl 				'\n'
-#define ff 					first
-#define ss 					second
-#define vi 					vector<int>
-#define pii 				pair<int, int>
-#define all(x)				(x).begin(), (x).end()
-#define rep(i, x, y)		for(int i=x; i<y; i++)
-#define setbits(x) 			__builtin_popcountll(x)
-#define ios ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-#define INF 				1e19
-#define mod 				1000000007 // 1e9+7
- 
-#define watch(x) cout<<(#x)<<" is "<<(x)<<"\n"
-#define watch2(x,y) cout<<(#x)<<" is "<<(x)<<" and "<<(#y)<< " is " <<(y)<<"\n"
- 
+
  using namespace std;
  
 const int N = 200005;
  
- bool myCompare(int a,int b)
-{
-  return a>b;
-}
+ 
 int arr[N];
 pair<int,int> st[4*N];//remember 4 times
  
