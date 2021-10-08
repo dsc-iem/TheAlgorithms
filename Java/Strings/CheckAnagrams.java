4872298c9c4264e3025d543ea92cdd9f9ca2bc6f
lonewolf235 <gagantomar235@gmail.com>
2021-10-08 21:13:08 +0530

    Changed anagrams java file code
    This code reduces the work of for-each loop present in the previous code.
    Added some more comments.
    The time complexity is almost the same but somewhat less than the previous code

--- Java/Strings/CheckAnagrams.java
+++ Java/Strings/CheckAnagrams.java
@@ -1,5 +1,3 @@
-package Strings;
-
 import java.util.HashMap;
 import java.util.Map;
 
@@ -7,46 +5,35 @@ import java.util.Map;
  * Two strings are anagrams if they are made of the same letters arranged differently (ignoring the
  * case).
  */
-public class CheckAnagrams {
-  public static void main(String[] args) {
-    assert isAnagrams("Silent", "Listen");
-    assert isAnagrams("This is a string", "Is this a string");
-    assert !isAnagrams("There", "Their");
-  }
+//This program checks if both the strings are anagram of each other or not.
+//Prints 'Yes' if they are else print 'No'
 
-  /**
-   * Check if two strings are anagrams or not
-   *
-   * @param s1 the first string
-   * @param s2 the second string
-   * @return {@code true} if two string are anagrams, otherwise {@code false}
-   */
-  public static boolean isAnagrams(String s1, String s2) {
-    int l1 = s1.length();
-    int l2 = s2.length();
-    s1 = s1.toLowerCase();
-    s2 = s2.toLowerCase();
-    Map<Character, Integer> charAppearances = new HashMap<>();
-
-    for (int i = 0; i < l1; i++) {
-      char c = s1.charAt(i);
-      int numOfAppearances = charAppearances.getOrDefault(c, 0);
-      charAppearances.put(c, numOfAppearances + 1);
+public class anagrams {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        String a=sc.nextLine();    //a is 1st String
+        a=a.toLowerCase();         //converting it to lower case
+        HashMap<Character,Integer> h=new HashMap<>(); // h is hashmap to store characters present in string a. 
+        for(int i=0;i<a.length();i++) {
+            char ch=a.charAt(i);
+            if(h.containsKey(ch))
+            h.put(ch, h.get(ch)+1);  //getting previous count of characters and adding 1 to it.
+            else
+            h.put(ch, 1);
+        }
+        String b=sc.nextLine();   //b is 2nd String
+        b=b.toLowerCase();        //converting it to lower case
+        HashMap<Character,Integer> h2=new HashMap<>();   // h2 is hashmap to store characters present in string b.
+        for(int i=0;i<b.length();i++) {
+            char ch=b.charAt(i);
+            if(h2.containsKey(ch))
+            h2.put(ch, h2.get(ch)+1);   //getting previous count of characters and adding 1 to it.
+            else
+            h2.put(ch, 1);
+        }
+        if(h.equals(h2))
+        System.out.println("Yes");
+        else
+        System.out.println("No");
     }
-
-    for (int i = 0; i < l2; i++) {
-      char c = s2.charAt(i);
-      if (!charAppearances.containsKey(c)) {
-        return false;
-      }
-      charAppearances.put(c, charAppearances.get(c) - 1);
-    }
-
-    for (int cnt : charAppearances.values()) {
-      if (cnt != 0) {
-        return false;
-      }
-    }
-    return true;
-  }
 }
