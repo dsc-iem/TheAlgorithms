c6194b889ac9c1f32a8ec8e117854565224da6fe
Arya Gupta <84087089+Arya-Gupta@users.noreply.github.com>
2021-10-24 00:05:49 +0530

    Add files via upload

---
+++ CPlusPlus/data_structures/cyclic_rotation.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+void rotate(int arr[], int n);
+
+int main()
+{
+    int n;
+    cout<<"Enter size of array: ";
+    cin>>n;
+    int arr[n];
+    cout<<"Enter the array:\n";
+    for(int i=0;i<n;i++)
+        cin>>arr[i];
+    rotate(arr,n);
+    return 0;
+}
+
+void rotate(int arr[], int n)
+{
+    int num=arr[n-1];
+    for(int i=n-2;i>=0;i--)
+    {
+        arr[i+1]=arr[i];
+    }
+    arr[0]=num;
+    cout<<"The new array is: ";
+    for(int i=0;i<n;i++)
+        cout<<arr[i]<<" ";
+}
