206314688dc5e7fde9dc6cfb20016503a19e84b6
Om Sharma <65007675+om-sharma03@users.noreply.github.com>
2021-10-23 20:17:11 +0530

    Create Inheritance.java

---
+++ Java/OOPS/Inheritance.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class one {
+    public void print_geek()
+    {
+        System.out.println("Geeks");
+    }
+}
+
+class two extends one {
+    public void print_for() { System.out.println("for"); }
+}
+
+class three extends two {
+    public void print_geek()
+    {
+        System.out.println("Geeks");
+    }
+}
+
+public class Inheritance {
+    public static void main(String[] args)
+    {
+        three g = new three();
+        g.print_geek();
+        g.print_for();
+        g.print_geek();
+    }
+}
