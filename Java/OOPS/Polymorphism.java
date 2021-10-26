734379da3f27b8af00f46296f1717ed2eb01ed9d
Om Sharma <65007675+om-sharma03@users.noreply.github.com>
2021-10-26 11:01:28 +0530

    Update Polymorphism.java

--- Java/OOPS/Polymorphism.java
+++ Java/OOPS/Polymorphism.java
@@ -1,12 +1,14 @@
+/*The word polymorphism means having many forms. In simple words, we can define polymorphism as the ability of a message to be displayed in more than one form.*/
+
 class Polymorphism{  
     public static void main(String args[]){  
         Bank b;  
-        b=new SBI();  
-        System.out.println("SBI Rate of Interest: "+b.getRateOfInterest());  
+        b=new SBI();  // Created an object of SBI
+        System.out.println("SBI Rate of Interest: "+b.getRateOfInterest());  // The getRateOfInterest function of SBI class is called
         b=new ICICI();  
-        System.out.println("ICICI Rate of Interest: "+b.getRateOfInterest());  
+        System.out.println("ICICI Rate of Interest: "+b.getRateOfInterest());  // The getRateOfInterest function of ICICI class is called
         b=new AXIS();  
-        System.out.println("AXIS Rate of Interest: "+b.getRateOfInterest());  
+        System.out.println("AXIS Rate of Interest: "+b.getRateOfInterest());  // The getRateOfInterest function of AXIS class is called
     }  
 }
 
@@ -25,3 +27,11 @@ class ICICI extends Bank{
 class AXIS extends Bank{  
     float getRateOfInterest(){return 9.7f;}  
 }    
+
+/*
+    OUTPUT:
+        SBI Rate of Interest: 8.4
+        ICICI Rate of Interest: 7.3
+        AXIS Rate of Interest: 9.7
+
+*/
