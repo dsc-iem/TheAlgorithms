87ad1dc9aecc03dac1c3d0a27ea3fd23e6272968
Om Sharma <65007675+om-sharma03@users.noreply.github.com>
2021-10-27 23:36:39 +0530

    Create Polymorphism.java

---
+++ Java/OOPS/Polymorphism.java
@@ -0,0 +1,30 @@
+/*The word polymorphism means having many forms.
+ * In simple words, we can define polymorphism as the ability of a message to be displayed in more than one form.*/
+
+class Polymorphism{  
+    public static void main(String args[]){  
+        Bank b;  
+        b=new SBI();  // Created an object of SBI
+        System.out.println("SBI Rate of Interest: "+b.getRateOfInterest());  // The getRateOfInterest function of SBI class is called
+        b=new ICICI();  
+        System.out.println("ICICI Rate of Interest: "+b.getRateOfInterest());  // The getRateOfInterest function of ICICI class is called
+        b=new AXIS();  
+        System.out.println("AXIS Rate of Interest: "+b.getRateOfInterest());  // The getRateOfInterest function of AXIS class is called
+    }  
+}
+
+class Bank{ 
+    float getRateOfInterest(){return 0;}  
+} 
+
+class SBI extends Bank{  
+    float getRateOfInterest(){return 8.4f;}  
+}  
+
+class ICICI extends Bank{  
+    float getRateOfInterest(){return 7.3f;}  
+}  
+
+class AXIS extends Bank{  
+    float getRateOfInterest(){return 9.7f;}  
+}    
