10eb924936c302cb01ffa6f5e960246c24af8342
SAI ROHIT CHAPPA <78897906+Rohit1173@users.noreply.github.com>
2021-10-29 12:27:44 +0530

    Update add_two_binary_strings.cpp

--- CPlusPlus/bit_manipulation/add_two_binary_strings.cpp
+++ CPlusPlus/bit_manipulation/add_two_binary_strings.cpp
@@ -1,8 +1,14 @@
+//Make them equal sized by adding 0s at the beginning of smaller string.
+// Perform bit addition
+//Time Complexity: O(log(m)+n1+n2) ; m->decimal representation of the sum of the binary numbers;n1->length of bit-1;n2->length of bit-2
+//Space Complexity:O(1)
 #include <bits/stdc++.h>
 using namespace std;
 
 string addBitStrings( string first, string second );
-
+// Helper method: given two unequal sized bit strings, converts them to
+// same length by adding leading 0s in the smaller string. Returns the
+// the new length
 int makeEqualLength(string &str1, string &str2)
 {
 	int len1 = str1.size();
@@ -25,6 +31,7 @@ int makeEqualLength(string &str1, string &str2)
 string addBitStrings( string first, string second )
 {
 	string result; 
+	// make the lengths same before adding
 	int length = makeEqualLength(first, second);
 
 	int carry = 0; 
@@ -33,11 +40,14 @@ string addBitStrings( string first, string second )
 	{
 		int firstBit = first.at(i) - '0';
 		int secondBit = second.at(i) - '0';
+		// boolean expression for sum of 3 bits
 		int sum = (firstBit ^ secondBit ^ carry)+'0';
 
 		result = (char)sum + result;
+		// boolean expression for 3-bit addition
 		carry = (firstBit & secondBit) | (secondBit & carry) | (firstBit & carry);
 	}
+	// if overflow, then add a leading 1
 	if (carry)
 		result = '1' + result;
 
